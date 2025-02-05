32c2da21e05797c402a33be3da422c88fae822a0
RV <rvbenu@gmail.com>
2025-02-05 16:56:25 -0500

    Wed Feb 5

---
+++ client.cpp
@@ -0,0 +1,23 @@
+#include <cstdio> 
+
+
+
+
+using manespace std;
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        fprintf(stdout, "Usage: <ip> <port> \n");
+        exit; 
+    }
+
+    int server_ip = atoi(argv[1]); 
+    int server_port = atoi(argv[2]); 
+
+    // Request connection to `server_ip` on port `server_port`. 
+    
+
+    
+
+}
